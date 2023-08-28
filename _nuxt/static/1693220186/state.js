dd47449ee0b16c8b7f4c97f2359cc7950befd290
Lukas Blecher <luk.blecher@gmail.com>
2023-08-28 12:57:24 +0200

    fix broken relative links

---
+++ _nuxt/static/1693220186/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnougat\u002F_nuxt\u002Fstatic\u002F1693220186",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnougat\u002F",assetsPath:"\u002Fnougat\u002F_nuxt\u002F",cdnURL:a}}}}(null));
