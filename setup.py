3818c1f92c8c567aa5cd18753d212e50b053e7b5
S1M0N38 <bertolottosimone@gmail.com>
2024-04-16 18:57:06 +0200

    fix transformers deps #213 #216

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     py_modules=["predict", "app", "train", "test"],
     python_requires=">=3.7",
     install_requires=[
-        "transformers>=4.25.1",
+        "transformers>=4.25.1,<=4.38.2",
         "timm==0.5.4",
         "orjson",
         "opencv-python-headless",
